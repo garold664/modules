4d88344482522fd2fd43dcab2decbefa58c75ab9
garold664 <garold664@gmail.com>
2016-03-10 18:20:27 +0300

    изменил размеры отступов в некоторых файлах

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,30 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync");
 
 gulp.task("style", function() {
-  gulp.src("less/style.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 1 version",
-        "last 2 Chrome versions",
-        "last 2 Firefox versions",
-        "last 2 Opera versions",
-        "last 2 Edge versions"
-      ]})
-    ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.reload({stream: true}));
+    gulp.src("less/style.less")
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(postcss([
+            autoprefixer({browsers: [
+                "last 1 version",
+                "last 2 Chrome versions",
+                "last 2 Firefox versions",
+                "last 2 Opera versions",
+                "last 2 Edge versions"
+            ]})
+        ]))
+        .pipe(gulp.dest("css"))
+        .pipe(server.reload({stream: true}));
 });
 
 gulp.task("serve", ["style"], function() {
-  server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    ui: false
-  });
+    server.init({
+        server: ".",
+        notify: false,
+        open: true,
+        ui: false
+    });
 
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+    gulp.watch("less/**/*.less", ["style"]);
+    gulp.watch("*.html").on("change", server.reload);
 });
